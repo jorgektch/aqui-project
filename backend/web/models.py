a3fae15eb40572db7b1733e005ab1b74fbc38fa6
sofia <ssarmiento088@gmail.com>
2025-06-04 12:53:35 -0500

    Update models

--- backend/web/models.py
+++ backend/web/models.py
@@ -25,3 +25,21 @@ class TypeDocument(models.Model):
     def __str__(self):
         return self.name
 
+
+# Classes w FK (1)
+
+class Department(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id_institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.name} - {self.id_institution.name}"
+    
+class District(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id_city = models.ForeignKey(City, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.name}, {self.id_city.name}"
