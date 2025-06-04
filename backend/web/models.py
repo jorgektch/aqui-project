9cb854f67f2461d357d0fa9c85a3fdaff3157996
sofia <ssarmiento088@gmail.com>
2025-06-04 12:33:52 -0500

    Create initial models

--- backend/web/models.py
+++ backend/web/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+# Classes w/o FK
+
+class City(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+class Institution(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50, db_index=True)
+    domain = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
+
+class TypeDocument(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
