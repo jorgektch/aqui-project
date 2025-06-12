140c73e93b8a055b70990a8c25e8794233916c0e
Adrian Palacios <adrianpalaciosaguilar@outlook.es>
2025-06-11 23:16:33 -0500

    Add layout structure, new components, and assets for the homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
-
+import Layout from './layout/layout'
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        // add layout component here
+
+        <Route path="/" element={<Layout />} >
+          <Route index element={<Home />} />
+          {/* Add more routes here as needed */}
+          {/* Example: <Route path="about" element={<About />} /> */}
+        </Route>
       </Routes>
     </BrowserRouter>
   )
