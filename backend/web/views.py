e8502e0092cda6709e5d6c9d8ce9b137b5f83f44
sofia <ssarmiento088@gmail.com>
2025-06-18 23:51:46 -0500

    Create initial views

--- backend/web/views.py
+++ backend/web/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import *
+from .serializers import *
 
-# Create your views here.
+class CityView(viewsets.ModelViewSet):
+    serializer_class = CitySerializer
+    queryset = City.objects.all()
+
+class InstitutionView(viewsets.ModelViewSet):
+    serializer_class = InstitutionSerializer
+    queryset = Institution.objects.all()
+
+class TypeDocumentView(viewsets.ModelViewSet):
+    serializer_class = TypeDocumentSerializer
+    queryset = TypeDocument.objects.all()
+
+class DepartmentView(viewsets.ModelViewSet):
+    serializer_class = DepartmentSerializer
+    queryset = Department.objects.all()
+
+class DistrictView(viewsets.ModelViewSet):
+    serializer_class = DistrictSerializer
+    queryset = District.objects.all()
+
+class DeliveryPointView(viewsets.ModelViewSet):
+    serializer_class = DeliveryPointSerializer
+    queryset = DeliveryPoint.objects.all()
