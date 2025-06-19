11cfb34a7ceaa0e1fae29d13cba0e212c59aa278
sofia <ssarmiento088@gmail.com>
2025-06-18 23:40:10 -0500

    Create initial serializers

---
+++ backend/web/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from .models import *
+
+class CitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = City
+        fields = '__all__'
+
+class InstitutionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Institution
+        fields = '__all__'
+
+class TypeDocumentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TypeDocument
+        fields = '__all__'
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Department
+        fields = '__all__'
+
+class DistrictSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = District
+        fields = '__all__'
+
+class DeliveryPointSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DeliveryPoint
+        fields = '__all__'
+
