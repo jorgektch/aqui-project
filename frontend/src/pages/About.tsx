a15366b0c63298b240bd832ad20fe8570420f7bc
haru <harucq8@gmail.com>
2025-06-18 22:44:04 -0500

    actualizacion about

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,8 +1,29 @@
 import aboutBG from '../assets/bg-about.png'
+import leaf from '../assets/leaf.png'
+
+
 const anuncios = [
-  { id: 1, title: 'Nuevo menú semanal disponible', content: 'Descubre nuestras nuevas opciones de menús para esta semana, con platos frescos y deliciosos.' },
-  { id: 2, title: 'Promoción de almuerzos', content: 'Ordena tu almuerzo hoy y obtén un 10% de descuento en tu primera compra.' },
-  { id: 3, title: 'Entrega gratuita', content: 'Disfruta de entrega gratuita en pedidos superiores a $50.' },
+  // Anuncios para el tablón
+  { 
+    id: 1, 
+    title: '¡Nuevo sorteo!', 
+    content: (
+      <>
+        Participa por un menú gratis completando el formulario&nbsp;
+        <a 
+          href="https://forms.gle/c5sAxxSeDi97qSyv5" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="text-blue-600 underline"
+        >
+          aquí
+        </a>
+        .
+      </>
+    )
+  },
+  { id: 2, title: 'Cancelación de servicio 18/06/2025', content: 'En el día miercoles 18 de junio no se ofrecerá atención por motivos de fuerza mayor' },
+  { id: 3, title: 'Entrega gratuita', content: 'Disfruta de entrega gratuita en pedidos superiores a S/20.' },
   { id: 4, title: 'Recetas saludables', content: 'Visita nuestro blog para encontrar recetas saludables y consejos de nutrición.' },
 ]
 
@@ -16,14 +37,15 @@ function About() {
             AQUI Menú es una empresa dedicada a la entrega de menús preparados, ofreciendo opciones frescas, balanceadas y accesibles directamente en la puerta de nuestros clientes. Nuestra misión es simplificar la rutina alimentaria, garantizando calidad, puntualidad y una experiencia gastronómica conveniente para estudiantes y demás consumidores. 
           </p>
           <a href="/carta" className="text-lg bg-neutral-800 flex items-center justify-center py-2 text-white mt-4 w-full">
-            Empezar
+            Ver tablón de anuncios
           </a>
         </div>
       </div>
       <div className="container mx-auto px-4 mt-8">
-        <div className='relative'>
-          <h2 className='text-2xl font-bold text-center'>Tablón</h2>
-        </div>
+        <div className='relative text-center mb-4'>
+         <img src={leaf} alt="Decoración menú" className="mx-auto mb-2 w-8 h-8" />
+         <h2 className='text-2xl font-bold'>Tablón</h2>
+      </div>
         <div className="grid grid-cols-1 gap-6 mt-6">
           <div className="col-span-1">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
