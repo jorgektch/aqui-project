d294dabb52e1147a937e2b47f4819f874060db8f
sofia <ssarmiento088@gmail.com>
2025-06-26 00:26:06 -0500

    Update urls

--- backend/web/urls.py
+++ backend/web/urls.py
@@ -4,11 +4,16 @@ from .views import *
 
 router = routers.DefaultRouter()
 router.register(r'cities', CityView, 'cities')
+router.register(r'food-categories', FoodCategoryView, 'food-categories')
 router.register(r'institutions', InstitutionView, 'institutions')
 router.register(r'types-document', TypeDocumentView, 'types-document')
 router.register(r'departments', DepartmentView, 'departments')
 router.register(r'districts', DistrictView, 'districts')
+router.register(r'foods', FoodCategoryView, 'foods')
 router.register(r'delivery-points', DeliveryPointView, 'delivery-points')
+router.register(r'orders', OrderView, 'orders')
+router.register(r'order-items', OrderItemView, 'order-items')
+router.register(r'additionals', AdditionalView, 'additionals')
 
 urlpatterns = [
     path('', include(router.urls))
