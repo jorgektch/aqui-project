c5d8251db8a201d5e2b2536b3cae7da4c90cf9d8
sofia <ssarmiento088@gmail.com>
2025-06-26 00:26:38 -0500

    Adding admin sites

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(City)
+admin.site.register(FoodCategory)
+admin.site.register(Institution)
+admin.site.register(TypeDocument)
+admin.site.register(Department)
+admin.site.register(District)
+admin.site.register(Food)
+admin.site.register(DeliveryPoint)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Additional)
