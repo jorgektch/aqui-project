cdf1de9679b9a687f9cf8b3221b18dacea12abe7
sofia <ssarmiento088@gmail.com>
2025-06-26 00:19:46 -0500

    Update views #2

--- backend/web/views.py
+++ backend/web/views.py
@@ -7,6 +7,10 @@ class CityView(viewsets.ModelViewSet):
     serializer_class = CitySerializer
     queryset = City.objects.all()
 
+class FoodCategoryView(viewsets.ModelViewSet):
+    serializer_class = FoodCategorySerializer
+    queryset = Food.objects.all()
+
 class InstitutionView(viewsets.ModelViewSet):
     serializer_class = InstitutionSerializer
     queryset = Institution.objects.all()
@@ -23,6 +27,22 @@ class DistrictView(viewsets.ModelViewSet):
     serializer_class = DistrictSerializer
     queryset = District.objects.all()
 
+class FoodView(viewsets.ModelViewSet):
+    serializer_class = FoodSerializer
+    queryset = Food.objects.all()
+
 class DeliveryPointView(viewsets.ModelViewSet):
     serializer_class = DeliveryPointSerializer
     queryset = DeliveryPoint.objects.all()
+
+class OrderView(viewsets.ModelViewSet):
+    serializer_class = OrderSerializer
+    queryset = Order.objects.all()
+
+class OrderItemView(viewsets.ModelViewSet):
+    serializer_class = OrderItemSerializer
+    queryset = OrderItem.objects.all()
+
+class AdditionalView(viewsets.ModelViewSet):
+    serializer_class = AdditionalSerializer
+    queryset = Additional.objects.all()
