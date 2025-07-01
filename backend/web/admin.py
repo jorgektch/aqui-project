51cd64390c6ca2fec87a69a79cb92664ecf6cadf
sofia <ssarmiento088@gmail.com>
2025-07-01 00:12:33 -0500

    Fix and update admin sites

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
 from .models import *
 
+admin.site.register(User)
 admin.site.register(City)
-admin.site.register(FoodCategory)
 admin.site.register(Institution)
+admin.site.register(MenuItemType)
+admin.site.register(Role)
 admin.site.register(TypeDocument)
+admin.site.register(Collaborator)
 admin.site.register(Department)
 admin.site.register(District)
-admin.site.register(Food)
+admin.site.register(MenuItem)
+admin.site.register(Customer)
 admin.site.register(DeliveryPoint)
+admin.site.register(MenuAvailability)
 admin.site.register(Order)
-admin.site.register(OrderItem)
-admin.site.register(Additional)
+admin.site.register(Bag)
+admin.site.register(BagItem)
